ef59b585bf0d59268a0da41683a8c04eab3fc1d5
Nishanthan <nishanthan.karunakaran@ebitaus.com>
2025-03-12 17:27:00 +0530

    setup: shared module to auth

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
-
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +13,8 @@ import { SignupComponent } from './pages/signup/signup.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
