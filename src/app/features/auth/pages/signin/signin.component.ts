1a23479566e61ff134ee020654be3dcab58677fb
Nishanthan <nishanthan.karunakaran@ebitaus.com>
2025-03-13 13:37:58 +0000

    performance: signin

--- src/app/features/auth/pages/signin/signin.component.ts
+++ src/app/features/auth/pages/signin/signin.component.ts
@@ -17,39 +17,39 @@ export class SigninComponent {
     otp: ['', [Validators.required, Validators.minLength(6)]],
   });
 
-  getFormError(field: string): string {
+  getFormError(field: string): string | null {
+    if (!this.submitted) return null;
+
     const control = this.loginForm.get(field);
+    if (!control) return null;
 
-    if (this.submitted) {
-      if (control?.hasError('required'))
-        return `${field !== 'otp'
-          ? this.helperService.toTitleCase(field)
-          : field.toUpperCase()
-          } is required`;
+    if (control.hasError('required'))
+      return `${field !== 'otp'
+        ? this.helperService.toTitleCase(field)
+        : field.toUpperCase()
+        } is required`;
 
-      if (control?.hasError('email')) return 'Invalid email format';
+    if (control.hasError('email')) return 'Invalid email format';
 
-      if (control?.hasError('minlength')) {
-        const minLength = control.errors?.['minlength'].requiredLength;
-        return `${field} must be at least ${minLength} characters`;
-      }
+    if (control.hasError('minlength')) {
+      const minLength = control.errors?.['minlength']?.requiredLength;
+      return `${field} must be at least ${minLength} characters`;
     }
-    return '';
+
+    return null;
   }
+
 
   submitLoginForm() {
     this.submitted = true;
     console.log('login form values', this.loginForm.value);
 
-    if (this.loginForm.invalid) {
-      console.log('Form is invalid');
-      this.loginForm.markAllAsTouched();
-      return;
-    }
+    if (this.loginForm.invalid) return;
 
     this.isLoading = true;
     setTimeout(() => {
       this.isLoading = false;
     }, 3000);
   }
+
 }
